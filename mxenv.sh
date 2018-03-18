f77761370bb280213544e60e7419b7e20497a9c7
chinakook <chinakook@msn.com>
2018-03-18 20:20:36 +0800

    add my sh env

---
+++ mxenv.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/home/kk/dev/cudnn/lib64:/opt/intel/compilers_and_libraries/linux/lib/intel64:/opt/intel/mkl/lib/intel64:/home/kk/dev/mx/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/kk/dev/mx/python
