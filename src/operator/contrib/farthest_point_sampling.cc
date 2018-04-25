ba1c81471338d7e172d0fc99ca20eb6571c65b04
ld <chinakook@msn.com>
2018-04-25 09:56:59 +0800

    cpu fps and gather_point op

--- src/operator/contrib/farthest_point_sampling.cc
+++ src/operator/contrib/farthest_point_sampling.cc
@@ -1,7 +1,79 @@
 #include "./farthest_point_sampling-inl.h"
 
 namespace mxnet {
+
+	void farthestpointsamplingKernel(int b, int n, int m, const float *dataset, float *temp, int *idxs)
+	{
+		//std::vector<float> dists(b*n);
+		// std::vector<int> dist_i(b*m);
+		//std::fill(dists.begin(), dists.end(), std::numeric_limits<float>::max());
+
+		
+		for (int i = 0; i < b; ++i)
+		{
+			float xprev = dataset[(i*n + 0)*3 + 0];
+			float yprev = dataset[(i*n + 0) * 3 + 1];
+			float zprev = dataset[(i*n + 0) * 3 + 2];
+			for (int k = 0; k < m; ++k)
+			{
+				float max = 0;
+				int jmax = 0;
+				for (int j = 0; j < n; ++j)
+				{
+					float x0 = dataset[(i*n + j) * 3 + 0];
+					float y0 = dataset[(i*n + j) * 3 + 1];
+					float z0 = dataset[(i*n + j) * 3 + 2];
+					float dist = (x0 - xprev)*(x0 - xprev) + (y0 - yprev)*(y0 - yprev) + (z0 - zprev)*(z0 - zprev);
+					
+					temp[(i*n + j)] = std::min(temp[(i*n + j)], dist);
+					if (temp[(i*n + j)] > max)
+					{
+						max = temp[(i*n + j)];
+						jmax = j;
+					}
+				}
+				xprev = dataset[(i*n + jmax) * 3 + 0];
+				yprev = dataset[(i*n + jmax) * 3 + 1];
+				zprev = dataset[(i*n + jmax) * 3 + 2];
+				idxs[i*m + k] = jmax;
+			}
+
+		}
+
+	}
+
 namespace op {
+
+	template<>
+	void FarthestPointSamplingCompute<cpu>(const nnvm::NodeAttrs& attrs,
+		const OpContext& ctx,
+		const std::vector<TBlob>& inputs,
+		const std::vector<OpReqType>& req,
+		const std::vector<TBlob>& outputs) {
+		using namespace mshadow;
+		CHECK_GT(ctx.requested.size(), 0);
+		const int B = inputs[0].size(0);
+		const int N = inputs[0].size(1);
+		const FarthestPointSamplingParam& param = nnvm::get<FarthestPointSamplingParam>(attrs.parsed);
+
+		auto *stream = ctx.get_stream<cpu>();
+		auto tmp_shape = mshadow::Shape2(32, N);
+
+		Tensor<cpu, 2, float> tmp = ctx.requested[0].get_space_typed<cpu, 2, float>(tmp_shape, stream);
+		Fill<false>(stream, TBlob(reinterpret_cast<nnvm::dim_t*>(tmp.dptr_), tmp_shape, gpu::kDevMask), kWriteTo, 1e10);
+
+		farthestpointsamplingKernel(
+			B, N, param.npoints, inputs[0].dptr<float>(), tmp.dptr_, outputs[0].dptr<int>());
+	}
+
+	template<>
+	void FarthestPointSamplingGradCompute<cpu>(const nnvm::NodeAttrs& attrs,
+		const OpContext& ctx,
+		const std::vector<TBlob>& inputs,
+		const std::vector<OpReqType>& req,
+		const std::vector<TBlob>& outputs) {
+
+	}
 
 DMLC_REGISTER_PARAMETER(FarthestPointSamplingParam);
 
--- src/operator/contrib/gather_point.cc
+++ src/operator/contrib/gather_point.cc
@@ -1,8 +1,67 @@
 #include "./gather_point-inl.h"
-
+#include <atomic>
 
 namespace mxnet {
+
+	void gatherpointKernel(int b, int n, int m, const float *inp, const int *idx, float *out)
+	{
+		for (int i = 0; i < b; ++i)
+		{
+			for (int j = 0; j < m; ++j)
+			{
+				int a = idx[i*m + j];
+				out[(i*m + j) * 3 + 0] = inp[(i*n + a) * 3 + 0];
+				out[(i*m + j) * 3 + 1] = inp[(i*n + a) * 3 + 1];
+				out[(i*m + j) * 3 + 2] = inp[(i*n + a) * 3 + 2];
+			}
+		}
+	}
+
+	void scatteraddpointKernel(int b, int n, int m, const float *out_g, const int *idx, float *inp_g)
+	{
+		for (int i = 0; i < b; ++i)
+		{
+			for (int j = 0; j < m; ++j)
+			{
+				int a = idx[i*m + j];
+				inp_g[(i*n + a) * 3 + 0] += out_g[(i*m + j) * 3 + 0];
+				inp_g[(i*n + a) * 3 + 1] += out_g[(i*m + j) * 3 + 1];
+				inp_g[(i*n + a) * 3 + 2] += out_g[(i*m + j) * 3 + 2];
+			}
+		}
+	}
+
 namespace op {
+
+	template<>
+	void GatherPointCompute<cpu>(const nnvm::NodeAttrs& attrs,
+		const OpContext& ctx,
+		const std::vector<TBlob>& inputs,
+		const std::vector<OpReqType>& req,
+		const std::vector<TBlob>& outputs) {
+		const int B = inputs[0].size(0);
+		const int N = inputs[0].size(1);
+		const int M = inputs[1].size(1);
+		gatherpointKernel(
+			B, N, M, inputs[0].dptr<float>(), inputs[1].dptr<int>(), outputs[0].dptr<float>());
+	}
+
+	template<>
+	void GatherPointGradCompute<cpu>(const nnvm::NodeAttrs& attrs,
+		const OpContext& ctx,
+		const std::vector<TBlob>& inputs,
+		const std::vector<OpReqType>& req,
+		const std::vector<TBlob>& outputs) {
+		const TBlob &out_grad = inputs[0];
+		const TBlob &idx = inputs[1];
+		const TBlob &in_grad = outputs[0];
+
+		const int B = in_grad.size(0);
+		const int N = in_grad.size(1);
+		const int M = idx.size(1);
+		scatteraddpointKernel(
+			B, N, M, out_grad.dptr<float>(), idx.dptr<int>(), in_grad.dptr<float>());
+	}
 
 NNVM_REGISTER_OP(_contrib_GatherPoint)
 .describe(R"code(aaaaa)code" ADD_FILELINE)
